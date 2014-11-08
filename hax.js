e174a27e6f3edb655499e2a229e0ee8c43129391
Damian Dziaduch <damian.dziaduch@gforces.pl>
2014-11-08 17:22:55 +0100

    New version. Removed filling the branch - we are now using Bitbucket and this is no longer required :).

--- hax.js
+++ hax.js
@@ -1,22 +1,18 @@
-var Hax = {
+(function (window, $) {
+    'use strict';
 
-    fixVersion: null,
-    isAuto: false,
-    
-    init: function () {
-        Hax.fixVersion = $.trim($('#fixfor-val').text());
-        Hax.project    = $('meta[name=ajs-issue-key]').attr('content').substr(0, $('meta[name=ajs-issue-key]').attr('content').indexOf('-')).toLowerCase();
-        Hax.isAuto     = Hax.project === 'autoten';
-    },
+    function Handler() {
+        var key = $('meta[name=ajs-issue-key]').attr('content');
+        this.project = key.substr(0, key.indexOf('-')).toLowerCase();
+    }
 
-    handleBtnClick: function () {
-        if (!Hax.isClickedButtonHandled(this)) {
-            return;
+    Handler.prototype.buttonHandler = function (button) {
+        if (this.isButtonHandled(button)) {
+            this.handleResolveButtonClick();
         }
-        Hax.handleResolveBtnClick();
-    },
+    };
 
-    isClickedButtonHandled : function (button) {
+    Handler.prototype.isButtonHandled = function (button) {
         switch ($('span', button).text()) {
             case 'Code Review':
             case 'Resolve to QA':
@@ -25,72 +21,51 @@ var Hax = {
             default:
                 return false;
         }
-    },
+    };
 
-    handleResolveBtnClick: function () {
+    Handler.prototype.handleResolveButtonClick = function () {
+        var handler = this;
         var interval = window.setInterval(function () {
-            if (!$('.jira-dialog').is(':visible')) return;
-            window.clearInterval(interval);
-            Hax.handleDialogOpen();
-        }, 250);
-    },
-
-    handleDialogOpen: function () {
-        Hax.fillDescription();
-        Hax.fillDepartment();
-    },
-
-    fillDescription: function () {
-        $('.jira-dialog-open .comment-input textarea').val(function () {
-            if ('None' === Hax.fixVersion) {
-                return Hax.getDetailedDescription();
-            } else {
-                return Hax.getMergedDescrption();
+            if ($('.jira-dialog').is(':visible')) {
+                window.clearInterval(interval);
+                handler.handleDialogOpen();
             }
-        });
-    },
+        }, 250);
+    };
 
-    getDetailedDescription: function () {
-        return ['Description', 'Replicate / Demonstrate', 'Affected Area(s)', 'Screenshot(s)', 'Settings / SQL', 'Branch'].map(function (name) {
-            return '*' + name + '*:\n' + ('Branch' === name ? Hax.getBranchName() : 'N/A\n\n');
+    Handler.prototype.handleDialogOpen = function () {
+        this.fillDescription();
+        this.fillDepartment();
+    };
+
+    Handler.prototype.fillDescription = function () {
+        $('.jira-dialog-open .comment-input textarea').val(
+            'None' === this.fixVersion ? this.getDetailedDescription() : this.getMergedDescrption()
+        );
+    };
+
+    Handler.prototype.getDetailedDescription = function () {
+        return [
+            'Description',
+            'Replicate / Demonstrate',
+            'Affected Area(s)',
+            'Screenshot(s)',
+            'Settings / SQL'
+        ].map(function (name) {
+            return '*' + name + '*:\n' + 'N/A\n\n';
         }).join('');
-    },
+    };
 
-    getBranchName: function () {
-        var id = $('meta[name=ajs-issue-key]').attr('content');
-        if (Hax.isAuto || 'ial' === Hax.project) {
-            return id;
-        }
-        if ('aljuli' === Hax.project) {
-            return 'branches/' + id;
-        }
-        return 'branches/' + Hax.getBranchDir() + '/trunk-jira-' + id.toLowerCase();
-    },
+    Handler.prototype.getMergedDescrption = function () {
+        return 'Merged.';
+    };
 
-    getBranchDir: function () {
-        switch ($.trim($('#type-val').text().toLowerCase())) {
-            case 'software defect':
-                return 'defects';
-            default:
-                return 'features';
-        }
-    },
+    Handler.prototype.fillDepartment = function () {
+        $('#' + $('.jira-dialog-open label:contains("Department")').attr('for')).val(this.getDepartment());
+    };
 
-    getMergedDescrption: function () {
-        return 'Merged to ' + Hax.getMergedTo() + '.';
-    },
-
-    getMergedTo: function () {
-        if (Hax.isAuto) return 'develop';
-        return 'version ' + Hax.fixVersion.replace(/(\d+\.\d+)+(\.\d+)?/, '$1');
-    },
-
-    fillDepartment: function () {
-        $('#' + $('.jira-dialog-open label:contains("Department")').attr('for')).val(Hax.getDepartment);
-    },
-
-    getDepartment: function () {
-        switch (Hax.project) {
+    Handler.prototype.getDepartment = function () {
+        switch (this.project) {
             case 'nmgboem': case 'net':
                 return 'QA - OEM';
             case 'std': case 'sun':
@@ -100,8 +75,11 @@ var Hax = {
             default:
                 return 'QA';
         }
-    }
-};
-
-Hax.init();
-$(document).on('click', '.issueaction-workflow-transition', Hax.handleBtnClick);
+    };
+    
+    var handler = new Handler();
+    
+    $('body').on('click', '.issueaction-workflow-transition', function () {
+        handler.buttonHandler(this);
+    });
+}(this, this.$));
